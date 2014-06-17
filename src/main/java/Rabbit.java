7fe7c6c0d1ff8f8bb2fb9db9e996a0a8f645010c
Miyazaki Motokatsu <motokatsu.miyazaki@tel.com>
2014-06-17 13:22:30 +0900

    Add Rabbit

---
+++ src/main/java/Rabbit.java
@@ -0,0 +1,14 @@
+
+/**
+ * うさぎのうさ (名前は変えられません) を生成します。
+ * そのうち抽象化されるかもしれません。
+ * @author Hiroyuki
+ *
+ */
+public class Rabbit {
+	private static String name = "Usa";
+	
+	public String getName() {
+		return Rabbit.name;
+	}
+}
