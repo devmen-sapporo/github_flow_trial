be4092b7d0f974e535d5afe6cffad0b7498fa544
a-suzuki <atsushisuzuki.mail@gmail.com>
2014-06-19 19:48:24 +0900

    Ctrl+Shift+Fでコード成形した

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,14 +1,12 @@
-
 /**
- * 犬のポチ (名前は変えられません) を生成します。
- * そのうち抽象化されるかもしれません。
+ * 犬のポチ (名前は変えられません) を生成します。 そのうち抽象化されるかもしれません。
  * 
  * @author mae0003
- *
+ * 
  */
 public class Dog {
 	private static String name = "Pochi";
-	
+
 	/**
 	 * get name
 	 * 
