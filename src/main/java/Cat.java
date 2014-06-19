6d6d691dbdfe92e88019aeb937a7a5e98e4c17b3
a-suzuki <atsushisuzuki.mail@gmail.com>
2014-06-19 18:17:29 +0900

    Add cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+/**
+ * 猫のネネ (名前は変えられません) を生成します。
+ * そのうち抽象化されるかもしれません。
+ *
+ */
+public class Cat {
+	private static String name = "Nene";
+	
+	public String getName() {
+		return Cat.name;
+	}
+}
